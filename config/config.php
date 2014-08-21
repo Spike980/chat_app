9539c76924758925529ecb03daf63376b66bd572
Anand Mallick <akm_spike@yahoo.com>
2014-08-21 18:21:36 +0530

    framework built

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+define('DB_NAME', 'chat');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'm3db35t');
+define('DB_HOST', 'localhost');
