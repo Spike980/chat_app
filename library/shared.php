783db47a1da334f7af508ccf9ab0ce3972b96d47
Anand Mallick <akm_spike@yahoo.com>
2014-08-21 18:25:37 +0530

    framework half done

--- library/shared.php
+++ library/shared.php
@@ -46,14 +46,16 @@ function callHook() {
 /** Autoload any classes that are required **/
 
 function __autoload($className) {
+	echo "$className";
 	if (file_exists(ROOT . DS . 'library' . DS . strtolower($className) . '.class.php')) {
-		require_once(ROOT . DS . 'library' . DS . strtolower($className) . '.class.php');
+		require_once (ROOT . DS . 'library' . DS . strtolower($className) . '.class.php');
 	} else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php')) {
-		require_once(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php');
+		require_once (ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php');
 	} else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . strtolower($className) . '.php')) {
-		require_once(ROOT . DS . 'application' . DS . 'models' . DS . strtolower($className) . '.php');
+		require_once (ROOT . DS . 'application' . DS . 'models' . DS . strtolower($className) . '.php');
 	} else {
 		/* Error Generation Code Here */
+		echo 'CLass Not Found';
 	}
 }
 
--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 
+if (isset($_GET['url']))
+{
 $url = $_GET['url'];
+echo "$url";
+}
 
 require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
